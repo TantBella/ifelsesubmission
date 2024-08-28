b48d45f51456a3dfdcffe5a73ec2b5b240255cd5
Bella Solo <bella.solomon-falkfrii@outlook.com>
2024-08-28 11:51:35 +0200

    first task done

---
+++ IfElseSubbmission/Program.cs
@@ -0,0 +1,18 @@
+﻿//1
+
+
+Console.WriteLine("Hello! Please write a number.");
+string evenOdd = Console.ReadLine();
+
+int evenOddNumber = Convert.ToInt32(evenOdd);
+if (evenOddNumber % 2 == 0)
+{
+    Console.WriteLine("You chose: " + evenOddNumber + ", which is an even number.");
+}
+else
+{
+    Console.WriteLine("You chose " + evenOdd + ", which is an odd number.");
+}
+
+
+//2
