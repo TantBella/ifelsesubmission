b76569aec22c6c8badf0a803b4ab6b4f18f5bc8d
Bella Solo <bella.solomon-falkfrii@outlook.com>
2024-08-28 12:07:28 +0200

    finished task nr 3

--- IfElseSubbmission/Program.cs
+++ IfElseSubbmission/Program.cs
@@ -17,17 +17,47 @@
 
 //2. Age bracket
 
-Console.WriteLine("How old are you?");
-string ageString = Console.ReadLine();
-int ageNumber = Convert.ToInt32(ageString);
-if (ageNumber < 13) {
-    Console.WriteLine("You are a child.");
-}
-if (ageNumber >= 13 && ageNumber <= 19)
+//Console.WriteLine("How old are you?");
+//string ageString = Console.ReadLine();
+//int ageNumber = Convert.ToInt32(ageString);
+//if (ageNumber < 13) {
+//    Console.WriteLine("You are a child.");
+//}
+//if (ageNumber >= 13 && ageNumber <= 19)
+//{
+//    Console.WriteLine("You're a teenager.");
+//}
+//else if (ageNumber > 19 )
+//{
+//    Console.WriteLine("You are an adult.");
+//}
+
+//3. Grade system
+
+Console.WriteLine("Please put in your test score.");
+string testScoreString = Console.ReadLine();
+
+int testScore = Convert.ToInt32(testScoreString);
+
+if (testScore >= 90 && testScore <= 100)
 {
-    Console.WriteLine("You're a teenager.");
+    Console.WriteLine("You got an A! Good job!");
 }
-else if (ageNumber > 19 )
+if (testScore >=80  && testScore <= 89)
 {
-    Console.WriteLine("You are an adult.");
+    Console.WriteLine("You got a B. Well done.");
 }
+if (testScore >= 70 && testScore <= 79)
+{
+    Console.WriteLine("You got a C. Keep practicing!");
+}
+if (testScore >= 60 && testScore <= 69)
+{
+    Console.WriteLine("You got a D. Keep practicing!");
+}
+else if (testScore < 60)
+{
+    Console.WriteLine("You got a F.. ");
+}
+
+//4. Leap year checker
